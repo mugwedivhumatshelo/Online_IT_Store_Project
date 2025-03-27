3fc28cd20057bd54cc6b9664c3caf151446a70ee
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2025-03-27 04:18:17 -0700

    Updated serializers.py for product and service

--- online_it_store/api/serializers.py
+++ online_it_store/api/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import User
+from .models import User, Product, Service
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password']
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = '__all__'
 
